80317eca94576adf864c56879d28963b95455b41
fatiperea <fatimaperea3pppp0@gmail.com>
2023-11-23 17:16:09 -0300

    ej de setInterval

--- js/apps.js
+++ js/apps.js
@@ -6,3 +6,11 @@ window.setTimeout(saludar, 5000)
 setTimeout(() =>{
     document.write("dia nublaado");
 }, 2000);
+
+const idIntervalo= setInterval(saludar, 2000);
+
+setInterval(() =>{
+    clearInterval(idIntervalo,10000);
+});
+
+//clearInterval(idIntervalo);
