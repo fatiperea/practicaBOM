67a2e90fec4e0fc8b3df2b3c280ba914f2390dac
fatiperea <fatimaperea3pppp0@gmail.com>
2023-11-26 22:04:27 -0300

    completo practica

--- js/apps.js
+++ js/apps.js
@@ -1,16 +1,20 @@
 function saludar(){
-    document.write("Hola mundo");
+    document.write("Hola mundo<br>");
 }
-window.setTimeout(saludar, 5000)
+window.setTimeout(saludar, 5000);
 
 setTimeout(() =>{
-    document.write("dia nublaado");
+    document.write("dia nublado<br>");
 }, 2000);
+
+//setInterval(saludar,2000);
 
 const idIntervalo= setInterval(saludar, 2000);
 
-setInterval(() =>{
+setTimeout(() =>{clearInterval(idIntervalo)}, 6000);
+
+/*setInterval(() =>{
     clearInterval(idIntervalo,10000);
 });
-
+*/
 //clearInterval(idIntervalo);
